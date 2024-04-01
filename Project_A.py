56f2e74a1b6b05574cba65bf29a912550a55f90c
Sekou M. Kamara <kamarasekou798@gmail.com>
2024-04-01 14:01:11 +0300

    Second edit

--- Project_A.py
+++ Project_A.py
@@ -1,10 +1,12 @@
 import random
 
+print("\nPlease make sure you have an identification number before creating an account.This helps us reaching you faster using numerical algorithm\n")
+
 
 def identity_generator():
-    full_name = input("\nPlease enter a prospective user name.\n")
+    full_name = input("\nPlease enter a prospective user name⬇️.\n")
     Idn_num = " "
-    rand_code = random.sample(range(0, 10, 1), 3)
+    rand_code = random.sample(range(0, 9, 1), 3)
     for i in rand_code:
         Idn_num += str(i)
     Idn_num = int(Idn_num)
@@ -13,15 +15,15 @@ def identity_generator():
 
     def inform_user():
         print(f"\n{full_name}, please use  both your identification number and prospective user name to  create a valid account.")
-        print("\n\nFormat: user_name =  Grade_Repository('user_name', idn_num)\nIdn stands for  identificcation")
+        print("\n\nFormat: user_name =  Projrct_A.account_creation('user_name', idn_num)\nIdn stands for  identificcation")
     inform_user()
     request_prom = input(
         "\nTo  understand our format of request, please input -h for help; else, any letter.\n")
     if request_prom == "-h":
-        print("\nThese helps  are useful  after you have created an account\n\nuser_name.add_stu: To add a student to the list.\nuser_name.:  Gives you several methods you have at your diposal.\nProject_A.grades_depositor: To input students grade in a file.")
+        print("\nThese helps are useful after you have created an account\n\nuser_name.add_stu: To add a student to the list.\nuser_name.:  Gives you several methods you have at your diposal.\n")
 
 
-class Account_creation:
+class account_creation:
     def __init__(self, user_name, Idn_num):
         self.User_Name = user_name
         self.Idn_num = Idn_num
@@ -32,7 +34,7 @@ class Account_creation:
     def add_stu(self):
         for i in self.student_num:
             self.stu_num += str(i)
-        student_name = input("\nPlease enter a student  name.\n")
+        student_name = input("\nPlease enter a student  name⬇️.\n")
         with open("stu_list", "a") as f:
             f.write(
                 f"name: {student_name} ---- admin#: {int(self.stu_num)}\n")
@@ -45,16 +47,16 @@ class Account_creation:
         else:
             print("You exit!")
 
+    class Grades_Depositor:
+        def __init__(self, stu_num, grade, subject):
+            self.stu_num = stu_num
+            self.grade = grade
+            self.subject = subject
 
-class Grades_Depositor:
-    def __init__(self, stu_num, grade, subject):
-        self.stu_num = stu_num
-        self.grade = grade
-        self.subject = subject
-
-        with open("grades_list", "a") as f:
-            f.write(
-                f"stu_num: {self.stu_num} -- grade: {self.grade} -- sub: {self.subject}\n")
-        with open("grades_list", "r") as f:
-            read_grade = f.read()
-        print(f"\nGrade successfully  added\nSee info below\n\n{read_grade}")
+            with open("grades_list", "a") as f:
+                f.write(
+                    f"stu_num: {self.stu_num} -- grade: {self.grade} -- sub: {self.subject}\n")
+            with open("grades_list", "r") as f:
+                read_grade = f.read()
+            print(
+                f"\nGrade successfully  added\nSee info below\n\n{read_grade}")
